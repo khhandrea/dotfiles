9b63ef21e1fd0ebffc2661238def0f251045ce1d
khhandrea <khhandrea@kakao.com>
2025-12-13 04:32:36 +0900

    Move to XDG_CONFIG_HOME

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
--- ~/.wezterm.lua
+-- ~/.config/wezterm/wezterm.lua
 
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
@@ -15,29 +15,11 @@ else
   config.default_prog = {"/bin/bash", "-l"}
 end
 
--- Font
-config.font = wezterm.font("D2Coding")
-config.font_size = 20
-config.default_cursor_style = "BlinkingBar"
-
--- config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "NeverPrompt"
-
-config.colors = {
-  foreground = "#D8D8D0", 
-  background = "#222420",
-	cursor_bg = "#50D0A0",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-}
-
-config.inactive_pane_hsb = {
-  saturation = 0.8,
-  brightness = 0.9,
-}
-
+-- Windows
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   left = 12,
   right = 4,
@@ -45,11 +27,18 @@ config.window_padding = {
   bottom = 12,
 }
 
--- opacity range [0, 1]
-config.window_background_opacity = 0.95
+-- Theme
+config.color_scheme = "Everforest Dark Hard (Gogh)"
 
--- blur range [0, 100]
-config.macos_window_background_blur = 10
+config.font = wezterm.font("D2Coding Nerd")
+config.font_size = 20
+
+config.default_cursor_style = "BlinkingBar"
+
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.9,
+}
 
 -- Keybindings
 local act = wezterm.action
